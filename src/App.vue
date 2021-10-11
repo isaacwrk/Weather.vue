a0cc7e4c409fabfeca9c4632c76040e2b639a4ee
Isaac Souza <isaacsouzawrk@gmail.com>
2021-10-11 13:18:59 -0300

    finishing style weather box

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@ main{
 
 .location-box, .date{
   color:#FFF;
-  font-size:26px;
+  font-size:20px;
   font-weight: 300;
   font-style: italic;
   text-align: center;
@@ -105,6 +105,29 @@ main{
 
 .weather-box{
   text-align:center;
+}
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box, .weather-box{
+  color:#FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
 </style>
