03cc941f64e46e9672a85e3eafd8724007006916
Isaac Souza <isaacsouzawrk@gmail.com>
2021-10-11 12:04:55 -0300

    creating style for search box

--- src/App.vue
+++ src/App.vue
@@ -32,5 +32,49 @@ body{
     font-family: 'montserrat', sans-serif;
 }
 
+#app{
+  background-image:url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main{
+  height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+  background-color:rgba(255,255,255,0.5);
+  padding: 15px;
+}
+
+.search-bar{
+  display:block;
+  width: 100%;
+  padding: 15px;
+  color:#313131;
+  font-size:20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
 </style>
 
